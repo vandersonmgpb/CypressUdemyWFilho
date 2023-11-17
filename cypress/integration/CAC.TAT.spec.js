f6cb7fbd0dfd15e1fe4b534537bb0b6eea4ae30d
vandersonmgpb <vanderson1022@gmail.com>
2023-11-17 20:15:37 -0300

    Selecionando opções de campos de seleção suspensa, aula 19

--- cypress/integration/CAC.TAT.spec.js
+++ cypress/integration/CAC.TAT.spec.js
@@ -79,10 +79,16 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       cy.get('.error').should('be.visible')
     })
 
-    it('envia o formuário com sucesso usando um comando customizado', function(){
+    it('Envia o formuário com sucesso usando um comando customizado', function(){
       cy.fillMandatoryFieldsAndSubmit()
 
       cy.get('.success').should('be.visible')
+    })
+
+    it.only('Seleciona um produto (YouTube) por seu texto', function(){
+      cy.get('#product')
+        .select('YouTube')
+        .should('have.value', 'youtube')
     })
   })
   
